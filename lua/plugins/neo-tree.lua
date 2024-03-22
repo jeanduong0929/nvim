d7f18b4fb6e71ce3d6cc67b2aca4d302307e534c
bduong <bduong@accuregsoftware.com>
2024-03-22 12:53:03 -0400

    Configured neotree to open explorer and reveal the current file

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,5 +1,8 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+    { "<leader>e", "<cmd>Neotree reveal=./<cr>", { noremap = true, silent = true } },
+  },
   config = function()
     require("neo-tree").setup({
       window = {

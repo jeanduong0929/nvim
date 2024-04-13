54c79ffda2e5b7aa34987f7ecebce95e86652199
Jean Duong <baoduongxy@gmail.com>
2024-04-12 23:43:40 -0400

    Automatically close neo-tree when opening a file.

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,6 +5,14 @@ return {
   },
   config = function()
     require("neo-tree").setup({
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function()
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
       window = {
         position = "right",
       },
